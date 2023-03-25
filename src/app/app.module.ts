efa8efd635bb5748d153f927485921d9aeb8c5b1
unknown <sanjaythirumangalath0@gmail.com>
2023-03-25 10:34:46 +0530

    news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeadingsComponent } from './headings/headings.component';
+import{HttpClientModule} from '@angular/common/http';
+import{NewsapiservicesService} from './service/newsapiservices.service';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { HealthComponent } from './health/health.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingsComponent,
+    EntertainmentComponent,
+    HealthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NewsapiservicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
